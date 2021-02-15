4c831fef25c553673f4c17e4170fb701c8c07edc
Vladimir Fominykh <vladimir3591f@mail.ru>
2021-02-15 15:50:23 +0300

    TDP1# Lab1 added

--- VLADFOM.ITMO.ADONET.ControlTasks/Lab1DataReader_Form.Designer.cs
+++ VLADFOM.ITMO.ADONET.ControlTasks/Lab1DataReader_Form.Designer.cs
@@ -29,10 +29,17 @@ namespace VLADFOM.ITMO.ADONET.ControlTasks
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Lab1DataReader_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(472, 238);
+            this.Name = "Lab1DataReader_Form";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
